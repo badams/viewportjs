28d22e1e4994f58913eb1197e721f42f3288b11a
byron.adams54@gmail.com <byron.adams54@gmail.com@001afc9e-f9ac-11de-8591-91bc733d3be8>
2010-01-05 09:46:47 +0000

    bouncing ball example

--- examples/basic/basic.js
+++ examples/basic/basic.js
@@ -5,16 +5,31 @@ function startDemo() {
   var viewport = new ViewPort('basic', 800, 600);
 
   // create an object to keep track of our coords
-  var player = {x : 100, y : 100};
+  var ball = {x : 100, y : 100, vel_x : 4, vel_y : 6,radius : 10};
 
   // in order to use doodle on our viewport we need to 
   // point it to the canvas element that viewport created.
   $doodle.canvas('#basic');
 
   viewport.draw(function () {
+
+    // check boundries
+    if ((this.width - ball.radius) < ball.x || (ball.x - ball.radius) < 0) {
+      ball.vel_x = ball.vel_x * -1;
+    }
+
+    if ((this.height - ball.radius) < ball.y || (ball.y - ball.radius) < 0) {
+      ball.vel_y = ball.vel_y * -1;
+    }
+
+    ball.x += ball.vel_x; 
+    ball.y += ball.vel_y;
+
+    this.canvas.clear(); // clear the screen.
+
     // this draws a circle where the 'player' should be. 
-    $doodle.circle({x : player.x, y : player.y, radius : 10, fill : 'red'}).draw();
+    $doodle.circle({x : ball.x, y : ball.y, radius : ball.radius, fill : 'red'}).draw();
   });
 
-  viewport.start();
+  viewport.start(); // nothing happens until this gets called
 };
